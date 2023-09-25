8573ef96a0694f6946948a0cd668d4dfd90eb01d
rimanshusingh4 <66876334+rimanshusingh4@users.noreply.github.com>
2023-09-25 16:04:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+gsap.to(".imageContainer", {
+    ease: Expo.easeInOut,
+    width: "100%",
+    stagger: 2,
+    repeat:-1
+    
+})
+
+gsap.to(".text h1", {
+    ease: Expo.easeInOut,
+    top: 0,
+    
+    stagger:2,
+    repeat:-1
+
+})
+
+gsap.to(".text h1", {
+    delay: 2,
+    ease: Expo.easeInOut,
+    top: "-100%",
+    stagger:2,
+    repeat:-1
+})
+// gsap.to(".bar i", {
+//     ease: Expo.easeInOut,
+//     top: 0,
+    
+//     stagger:2,
+//     repeat:-1
+
+// })
+
+
+// for breadcrumb.html
+
+// gsap.to(".breadline",{
+//     x:7,
+//     ease: Expo.easeInOut,
+//     stagger :1,
+//     repeat:-1
+    
+    
+
+// })
+// gsap.from(".breadline",{
+//     x:6,
+//     ease: Expo.easeInOut,
+//     stagger :1
+
